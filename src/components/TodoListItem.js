a8cbe85dc97755a985aff720a728beeed883b449
odinrossy <remniov.work@gamil.com>
2019-10-25 12:25:56 +0300

    Lesson 31. Maps and keys

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const TodoListItem = ({ text, important = false }) => {
     const style = {
         color: important ? 'tomato' : '#333333',
-        'font-weight': important ? 'bold' : 'normal'
+        'fontWeight': important ? 'bold' : 'normal'
     }
     return (
         <span style={style}>{text}</span>
