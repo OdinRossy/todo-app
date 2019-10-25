45d7ab92b85f2087847bf018d8ebf813f87504ef
odinrossy <remniov.work@gamil.com>
2019-10-25 13:15:01 +0300

    Updates

---
+++ precache-manifest.ac30abe26486fd16fe9a43819253d169.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644c6c4d3bcd1ccb480ec05fffb2f782",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "f089e82d7b0e46a94103",
+    "url": "/Todo-app/static/css/main.c96055c5.chunk.css"
+  },
+  {
+    "revision": "185ebd08c7e3e2eb2e17",
+    "url": "/Todo-app/static/js/2.180779c6.chunk.js"
+  },
+  {
+    "revision": "f089e82d7b0e46a94103",
+    "url": "/Todo-app/static/js/main.111604fd.chunk.js"
+  },
+  {
+    "revision": "8eca1fc6304c96112cca",
+    "url": "/Todo-app/static/js/runtime-main.afade4a5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo-app/precache-manifest.2873db6de0ff70413b93b0c001e5751e.js"
+  "/Todo-app/precache-manifest.ac30abe26486fd16fe9a43819253d169.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
